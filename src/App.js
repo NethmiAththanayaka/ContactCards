4e98ad04740a59e6755758d42edffa6837c70ee0
NethmiAththanayaka <jayasekaraamnp.19@uom.lk>
2022-09-13 12:22:12 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import AddContact from "./Components/AddContact";
+import ContactList from "./Components/ContactList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello world ! this is my new app profile page</h1>
+        <h1> Hello world ! </h1>
+        <Header/>
       </header>
     </div>
   );
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='ui menu'>
+        <div className='ui container'>
+            <h2>contact manager</h2>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
